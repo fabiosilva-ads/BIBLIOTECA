31e2a438b14356206eb62d970684392df40c43a5
Fábio Luiz da Silva <1911550205@fema.edu.br>
2020-05-31 19:46:36 -0300

    Camada DAL Select Form1

---
+++ BIBLIOTECA/CAMADAS/DAL/Clientes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIBLIOTECA.CAMADAS.DAL
+{
+    public class Clientes
+    {
+        private string strCon = Conexao.getConexao();
+
+        public List<MODEL.Clientes> Select()
+        {
+            List<MODEL.Clientes> lstClientes = new List<MODEL.Clientes>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "SELECT * FROM Clientes;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            try
+            {
+                conexao.Open();
+                SqlDataReader dados = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dados.Read())
+                {
+                    MODEL.Clientes cliente = new MODEL.Clientes();
+                    cliente.id = Convert.ToInt32(dados["id"].ToString());
+                    cliente.nome = dados["nome"].ToString();
+                    cliente.curso = dados["curso"].ToString();
+                    cliente.dias = Convert.ToInt32(dados["dias"].ToString());
+                    cliente.multa = Convert.ToSingle(dados["multa"].ToString());
+                    lstClientes.Add(cliente);
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na consulta de Clientes...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            return lstClientes;
+        }
+    }
+}
--- BIBLIOTECA/Form1.cs
+++ BIBLIOTECA/Form1.cs
@@ -16,5 +16,11 @@ namespace BIBLIOTECA
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CAMADAS.DAL.Clientes dalCli = new CAMADAS.DAL.Clientes();
+            dataGridView1.DataSource = dalCli.Select();
+        }
     }
 }
